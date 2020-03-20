7e95fb7caac6d704203c1a9548c42c03433dd29f
Mauro de Carvalho <mauro.dcg@gmail.com>
2020-03-20 16:04:55 -0300

    Bin2Dec - Golang

---
+++ golang_version/src/bin2dec.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Printf("Welcome to Bin2Dec!\nTo exit, press 'CTRL + C'.\n\n")
+	loop()
+}
+
+func convertBinaryToDecimal(binaryValue string) (int64, error) {
+	convertedBinaryToDecimal, err := strconv.ParseInt(binaryValue, 2, 32)
+	return convertedBinaryToDecimal, err
+}
+
+func loop() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		binaryValue := userInput(reader)
+		convertedBinarytoDecimal, err := convertBinaryToDecimal(binaryValue)
+		if err != nil {
+			fmt.Printf("\nThe value '%s' is not a binary. Try again.\n\n", binaryValue)
+			continue
+		}
+		fmt.Printf("\nBinary value: %s", binaryValue)
+		fmt.Printf("\nDecimal value: %d\n\n", convertedBinarytoDecimal)
+	}
+}
+
+func userInput(reader *bufio.Reader) string {
+	fmt.Println("What value do you want to convert from binary to decimal? ")
+	input, _ := reader.ReadString('\n')
+	return strings.Replace(input, "\n", "", -1)
+}
